e97e4d3b3e6c6221bee4f46a8486d21e2188616b
Administrator <Administrator@205-11>
2021-11-09 15:25:27 +0900

    update

---
+++ horse2직접구현/src/horse2직접구현/Horse.java
@@ -0,0 +1,17 @@
+package horse2직접구현;
+
+import javax.swing.ImageIcon;
+
+public class Horse {
+
+	public final int RUN = 0;
+	public final int GOAL = 1;
+
+	public int num;
+	public int x, y, w, h;
+
+	public String filename;
+	public ImageIcon image;
+
+	public int rank;
+}
